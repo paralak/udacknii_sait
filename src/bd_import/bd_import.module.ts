a03abdba9f40a8d2d74b6c0e9ee4924734663dee
paralak <paralakmc@gmail.com>
2025-11-19 19:27:51 +0300

    Update bd_import.module.ts

--- src/bd_import/bd_import.module.ts
+++ src/bd_import/bd_import.module.ts
@@ -3,9 +3,10 @@ import {Bd_importController} from './bd_import.controller';
 import {Bd_importService} from './bd_import.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Item } from 'src/db/item.entity';
+import { Stock } from 'src/db/stock.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Item])],
+    imports: [TypeOrmModule.forFeature([Item, Stock])],
     controllers: [Bd_importController],
     providers: [Bd_importService],
 })
