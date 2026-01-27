b6ccef4d1d8fbb4fdf6cb50577803918ce5dd122
paralak <paralakmc@gmail.com>
2026-01-27 15:58:54 +0300

    Update db.module.ts

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -6,8 +6,9 @@ import { Peremesh } from './peremesh.entity';
 import { Spisania } from './spisania.entity';
 import { Sales } from './sales.entity';
 import { Postavki } from './postavki.entity';
+import { Token } from './token.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Item, Stock, Peremesh, Spisania, Sales, Postavki])]
+  imports: [TypeOrmModule.forFeature([Item, Stock, Peremesh, Spisania, Sales, Postavki, Token])]
 })
 export class DBModule {}
