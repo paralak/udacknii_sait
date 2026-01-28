fd015972c4e796f5a0f212eaf9eb3b9bc5845aa6
paralak <paralakmc@gmail.com>
2026-01-28 16:56:00 +0300

    Update client_api.controller.ts

--- src/client_api/client_api.controller.ts
+++ src/client_api/client_api.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, Headers} from '@nestjs/common';
 import {ClientAPIService} from './client_api.service';
 import {Query} from '@nestjs/common';
 
@@ -13,10 +13,12 @@ export class ClientAPIController {
     }
 
     @Get('get_hierarchy_tree')
-    getHierarchyTree() {
+    getHierarchyTree(@Headers() headers: Record<string, string>) {
+        // Логируем заголовки запроса
+        console.log(JSON.stringify(headers, null, 2));
+        
         return this.clientAPIService.getHierarchyTree();
     }
-
     @Get('check_token')
         checkToken(@Query('token') token: string) {
             if (!token) {
