835e78a6ca2dced968994286a422ecd5befcb7fe
paralak <paralakmc@gmail.com>
2026-02-11 10:59:45 +0300

    Update personal.module.ts

--- src/client_api/personal/personal.module.ts
+++ src/client_api/personal/personal.module.ts
@@ -1,8 +1,5 @@
 import {Module} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Hierarchy } from 'src/db/hierarchy.entity';
-import { Token } from 'src/db/token.entity';
-import { Addresses } from 'src/db/addresses.entity';
 import { PersonalController } from './personal.controller';
 import { PersonalService } from './personal.service';
 import { Personal_ls } from 'src/db/personal/personal_ls.entity';
@@ -10,7 +7,7 @@ import { Personal_ls_info } from 'src/db/personal/personal_ls_info.entity';
 import { Personal_pos } from 'src/db/personal/personal_pos.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Token, Personal_ls, Personal_ls_info, Personal_pos])],
+    imports: [TypeOrmModule.forFeature([Personal_ls, Personal_ls_info, Personal_pos])],
     controllers: [PersonalController],
     providers: [PersonalService],
 })
