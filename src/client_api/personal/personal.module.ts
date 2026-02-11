a8a440e3fc555701d6745d99eb6de22b1a81df2f
paralak <paralakmc@gmail.com>
2026-02-11 10:56:54 +0300

    213

--- src/client_api/personal/personal.module.ts
+++ src/client_api/personal/personal.module.ts
@@ -5,9 +5,12 @@ import { Token } from 'src/db/token.entity';
 import { Addresses } from 'src/db/addresses.entity';
 import { PersonalController } from './personal.controller';
 import { PersonalService } from './personal.service';
+import { Personal_ls } from 'src/db/personal/personal_ls.entity';
+import { Personal_ls_info } from 'src/db/personal/personal_ls_info.entity';
+import { Personal_pos } from 'src/db/personal/personal_pos.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Hierarchy, Token, Addresses])],
+    imports: [TypeOrmModule.forFeature([Token, Personal_ls, Personal_ls_info, Personal_pos])],
     controllers: [PersonalController],
     providers: [PersonalService],
 })
