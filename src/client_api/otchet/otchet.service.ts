59937544dfc1119a778e322e1a28b0f093fa178d
paralak <paralakmc@gmail.com>
2026-02-27 12:05:26 +0300

    Update otchet.service.ts

--- src/client_api/otchet/otchet.service.ts
+++ src/client_api/otchet/otchet.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { M_for_zarplati } from 'src/db/m_for_zarplati.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class OtchetService {
     constructor(
-        private readonly mForZarplatiRepository: Repository<M_for_zarplati>,
+        @InjectRepository(M_for_zarplati)
+        private mForZarplatiRepository: Repository<M_for_zarplati>,
     ) {}
 
     async getMonths(hid: number) {
         let r = await this.mForZarplatiRepository.find({
             where: { hid: hid }
         });
-        //нужно вывести ответ со списком месяцов и ошибку если не найдено
         if (!r || r.length === 0) {
             return {
                 status: 'error',
